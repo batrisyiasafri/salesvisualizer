a447a6da2fedbba6e53d098239e1ad47d9260948
Batrisyia Najihah Safri <98628318+batrisyiasafri@users.noreply.github.com.>
2025-08-07 08:12:14 +0800

    updated app.py

--- app.py
+++ app.py
@@ -823,5 +823,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         create_admin_user()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True) 
+    # false if in production
 
